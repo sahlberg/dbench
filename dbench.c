8215d06a06d189d8216d0692580b96f10168d7ef
Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>
2018-03-28 14:37:44 +0900

    style: fix indentation and trailing spaces
    My editor (kate) is setup to clean up all trailing spaces and
    it can differentiate between spaces and tab marks as well.
    
    Signed-off-by: Daniel Sangorrin <daniel.sangorrin@toshiba.co.jp>

--- dbench.c
+++ dbench.c
@@ -1,17 +1,17 @@
-/* 
+/*
    Copyright (C) by Andrew Tridgell <tridge@samba.org> 1999-2007
    Copyright (C) 2001 by Martin Pool <mbp@samba.org>
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
@@ -144,34 +144,34 @@ static void sig_alarm(int sig)
 
         if (in_warmup) {
 		if (options.machine_readable) {
-                    printf("@W@%d@%d@%.2f@%u@%.03f@\n", 
-                       num_active, total_lines/nclients, 
-			   1.0e-6 * total_bytes / t, (int)t, latency*1000);
+			printf("@W@%d@%d@%.2f@%u@%.03f@\n",
+				num_active, total_lines/nclients,
+				1.0e-6 * total_bytes / t, (int)t, latency*1000);
 		} else {
-                    printf("%4d  %8d  %7.2f MB/sec  warmup %3.0f sec  latency %.03f ms\n", 
-                       num_active, total_lines/nclients, 
-			   1.0e-6 * total_bytes / t, t, latency*1000);
+			printf("%4d  %8d  %7.2f MB/sec  warmup %3.0f sec  latency %.03f ms\n",
+				num_active, total_lines/nclients,
+				1.0e-6 * total_bytes / t, t, latency*1000);
 		}
         } else if (in_cleanup) {
 		if (options.machine_readable) {
-                    printf("@C@%d@%d@%.2f@%u@%.03f@\n", 
-                       num_active, total_lines/nclients, 
-			   1.0e-6 * total_bytes / t, (int)t, latency*1000);
+			printf("@C@%d@%d@%.2f@%u@%.03f@\n",
+				num_active, total_lines/nclients,
+				1.0e-6 * total_bytes / t, (int)t, latency*1000);
 		} else {
-                    printf("%4d  cleanup %3.0f sec\n", nclients - num_finished, t);
+			printf("%4d  cleanup %3.0f sec\n", nclients - num_finished, t);
 		}
-        } else {
+	} else {
 		if (options.machine_readable) {
-                    printf("@R@%d@%d@%.2f@%u@%.03f@\n", 
-                       num_active, total_lines/nclients, 
-			   1.0e-6 * total_bytes / t, (int)t, latency*1000);
+			printf("@R@%d@%d@%.2f@%u@%.03f@\n",
+				num_active, total_lines/nclients,
+				1.0e-6 * total_bytes / t, (int)t, latency*1000);
 		} else {
-                    printf("%4d  %8d  %7.2f MB/sec  execute %3.0f sec  latency %.03f ms\n", 
-                       nclients, total_lines/nclients, 
-                       1.0e-6 * total_bytes / t, t, latency*1000);
-	  	       throughput = 1.0e-6 * total_bytes / t;
+			printf("%4d  %8d  %7.2f MB/sec  execute %3.0f sec  latency %.03f ms\n",
+				nclients, total_lines/nclients,
+				1.0e-6 * total_bytes / t, t, latency*1000);
+				throughput = 1.0e-6 * total_bytes / t;
 		}
-        }
+	}
 
 	fflush(stdout);
 next:
@@ -192,14 +192,14 @@ static void show_one_latency(struct op *ops, struct op *ops_all)
 		if (op_all->count == 0) continue;
 		if (options.machine_readable) {
 			printf(":%s:%u:%.03f:%.03f:\n",
-				       nb_ops->ops[i].name, op1->count, 
-				       1000*op1->total_time/op1->count,
-				       op1->max_latency*1000);
+				nb_ops->ops[i].name, op1->count,
+				1000*op1->total_time/op1->count,
+				op1->max_latency*1000);
 		} else {
 			printf(" %-22s %7u %9.03f %9.03f\n",
-				       nb_ops->ops[i].name, op1->count, 
-				       1000*op1->total_time/op1->count,
-				       op1->max_latency*1000);
+				nb_ops->ops[i].name, op1->count,
+				1000*op1->total_time/op1->count,
+				op1->max_latency*1000);
 		}
 	}
 	printf("\n");
@@ -232,9 +232,9 @@ static void report_latencies(void)
 	printf("Per client results:\n");
 	for (i=0;i<options.nprocs * options.clients_per_process;i++) {
 		child = &children[i];
-		printf("Client %u did %u lines and %.0f bytes\n", 
-		       i, child->line, child->bytes - child->bytes_done_warmup);
-		show_one_latency(child->ops, sum);		
+		printf("Client %u did %u lines and %.0f bytes\n",
+			i, child->line, child->bytes - child->bytes_done_warmup);
+		show_one_latency(child->ops, sum);
 	}
 }
 
@@ -336,7 +336,7 @@ static void create_procs(int nprocs, void (*fn)(struct child_struct *, const cha
 		}
 		if (WEXITSTATUS(status) != 0) {
 			printf("Child failed with status %d\n",
-			       WEXITSTATUS(status));
+				WEXITSTATUS(status));
 			exit(1);
 		}
 		i++;
@@ -358,68 +358,68 @@ static void process_opts(int argc, const char **argv)
 	int extra_argc = 0;
 	struct poptOption popt_options[] = {
 		POPT_AUTOHELP
-		{ "backend", 'B', POPT_ARG_STRING, &options.backend, 0, 
+		{ "backend", 'B', POPT_ARG_STRING, &options.backend, 0,
 		  "dbench backend (fileio, sockio, nfs, scsi, iscsi, smb)", "string" },
-		{ "timelimit", 't', POPT_ARG_INT, &options.timelimit, 0, 
+		{ "timelimit", 't', POPT_ARG_INT, &options.timelimit, 0,
 		  "timelimit", "integer" },
-		{ "loadfile",  'c', POPT_ARG_STRING, &options.loadfile, 0, 
+		{ "loadfile",  'c', POPT_ARG_STRING, &options.loadfile, 0,
 		  "loadfile", "filename" },
-		{ "directory", 'D', POPT_ARG_STRING, &options.directory, 0, 
+		{ "directory", 'D', POPT_ARG_STRING, &options.directory, 0,
 		  "working directory", NULL },
-		{ "tcp-options", 'T', POPT_ARG_STRING, &options.tcp_options, 0, 
+		{ "tcp-options", 'T', POPT_ARG_STRING, &options.tcp_options, 0,
 		  "TCP socket options", NULL },
-		{ "target-rate", 'R', POPT_ARG_DOUBLE, &options.targetrate, 0, 
+		{ "target-rate", 'R', POPT_ARG_DOUBLE, &options.targetrate, 0,
 		  "target throughput (MB/sec)", NULL },
-		{ "sync", 's', POPT_ARG_NONE, &options.sync_open, 0, 
+		{ "sync", 's', POPT_ARG_NONE, &options.sync_open, 0,
 		  "use O_SYNC", NULL },
-		{ "sync-dir", 'S', POPT_ARG_NONE, &options.sync_dirs, 0, 
+		{ "sync-dir", 'S', POPT_ARG_NONE, &options.sync_dirs, 0,
 		  "sync directory changes", NULL },
-		{ "fsync", 'F', POPT_ARG_NONE, &options.do_fsync, 0, 
+		{ "fsync", 'F', POPT_ARG_NONE, &options.do_fsync, 0,
 		  "fsync on write", NULL },
-		{ "xattr", 'x', POPT_ARG_NONE, &options.ea_enable, 0, 
+		{ "xattr", 'x', POPT_ARG_NONE, &options.ea_enable, 0,
 		  "use xattrs", NULL },
-		{ "no-resolve", 0, POPT_ARG_NONE, &options.no_resolve, 0, 
+		{ "no-resolve", 0, POPT_ARG_NONE, &options.no_resolve, 0,
 		  "disable name resolution simulation", NULL },
-		{ "clients-per-process", 0, POPT_ARG_INT, &options.clients_per_process, 0, 
+		{ "clients-per-process", 0, POPT_ARG_INT, &options.clients_per_process, 0,
 		  "number of clients per process", NULL },
-		{ "trunc-io", 0, POPT_ARG_INT, &options.trunc_io, 0, 
+		{ "trunc-io", 0, POPT_ARG_INT, &options.trunc_io, 0,
 		  "truncate all io to this size", NULL },
-		{ "one-byte-write-fix", 0, POPT_ARG_NONE, &options.one_byte_write_fix, 0, 
+		{ "one-byte-write-fix", 0, POPT_ARG_NONE, &options.one_byte_write_fix, 0,
 		  "try to fix 1 byte writes", NULL },
-		{ "stat-check", 0, POPT_ARG_NONE, &options.stat_check, 0, 
+		{ "stat-check", 0, POPT_ARG_NONE, &options.stat_check, 0,
 		  "check for pointless calls with stat", NULL },
-		{ "fake-io", 0, POPT_ARG_NONE, &options.fake_io, 0, 
+		{ "fake-io", 0, POPT_ARG_NONE, &options.fake_io, 0,
 		  "fake up read/write calls", NULL },
-		{ "skip-cleanup", 0, POPT_ARG_NONE, &options.skip_cleanup, 0, 
+		{ "skip-cleanup", 0, POPT_ARG_NONE, &options.skip_cleanup, 0,
 		  "skip cleanup operations", NULL },
-		{ "per-client-results", 0, POPT_ARG_NONE, &options.per_client_results, 0, 
+		{ "per-client-results", 0, POPT_ARG_NONE, &options.per_client_results, 0,
 		  "show results per client", NULL },
-		{ "server",  0, POPT_ARG_STRING, &options.server, 0, 
+		{ "server",  0, POPT_ARG_STRING, &options.server, 0,
 		  "server", NULL },
-		{ "nfs",  0, POPT_ARG_STRING, &options.nfs, 0, 
+		{ "nfs",  0, POPT_ARG_STRING, &options.nfs, 0,
 		  "nfs url", NULL },
 		{ "enable-nlm", 0, POPT_ARG_NONE, &options.nlm, 0,
 		  "Enable support for NFS byte range locking commands", NULL},
 		{ "run-once", 0, POPT_ARG_NONE, &options.run_once, 0,
 		  "Stop once reaching the end of the loadfile", NULL},
-		{ "scsi",  0, POPT_ARG_STRING, &options.scsi_dev, 0, 
+		{ "scsi",  0, POPT_ARG_STRING, &options.scsi_dev, 0,
 		  "scsi device", NULL },
 		{ "allow-scsi-writes", 0, POPT_ARG_NONE, &options.allow_scsi_writes, 0,
 		  "Allow SCSI write command to the device", NULL},
 #ifdef HAVE_LIBISCSI
-		{ "iscsi",  0, POPT_ARG_STRING, &options.iscsi_device, 0, 
+		{ "iscsi",  0, POPT_ARG_STRING, &options.iscsi_device, 0,
 		  "iscsi URL for the target device", NULL },
-		{ "iscsi-initiatorname",  0, POPT_ARG_STRING, &options.iscsi_initiatorname, 0, 
+		{ "iscsi-initiatorname",  0, POPT_ARG_STRING, &options.iscsi_initiatorname, 0,
 		  "iscsi InitiatorName", NULL },
 #endif
-		{ "warmup", 0, POPT_ARG_INT, &options.warmup, 0, 
+		{ "warmup", 0, POPT_ARG_INT, &options.warmup, 0,
 		  "How many seconds of warmup to run", NULL },
 		{ "machine-readable", 0, POPT_ARG_NONE, &options.machine_readable, 0,
 		  "Print data in more machine-readable friendly format", NULL},
 #ifdef HAVE_LIBSMBCLIENT
-		{ "smb-share",  0, POPT_ARG_STRING, &options.smb_share, 0, 
+		{ "smb-share",  0, POPT_ARG_STRING, &options.smb_share, 0,
 		  "//SERVER/SHARE to use", NULL },
-		{ "smb-user",  0, POPT_ARG_STRING, &options.smb_user, 0, 
+		{ "smb-user",  0, POPT_ARG_STRING, &options.smb_user, 0,
 		  "User to authenticate as : [<domain>/]<user>%<password>", NULL },
 #endif
 		POPT_TABLEEND
@@ -434,7 +434,7 @@ static void process_opts(int argc, const char **argv)
 			poptPrintHelp(pc, stdout, 0);
 			exit(1);
 		}
-		fprintf(stderr, "Invalid option %s: %s\n", 
+		fprintf(stderr, "Invalid option %s: %s\n",
 			poptBadOption(pc, 0), poptStrerror(opt));
 		exit(1);
 	}
@@ -458,7 +458,7 @@ static void process_opts(int argc, const char **argv)
 		exit(1);
 	}
 #endif
-	
+
 	options.nprocs = atoi(extra_argv[0]);
 
 	if (extra_argc >= 2) {
@@ -530,8 +530,8 @@ static void process_opts(int argc, const char **argv)
 		}
 	}
 
-        printf("Running for %d seconds with load '%s' and minimum warmup %d secs\n", 
-               options.timelimit, options.loadfile, options.warmup);
+	printf("Running for %d seconds with load '%s' and minimum warmup %d secs\n",
+		options.timelimit, options.loadfile, options.warmup);
 
 	create_procs(options.nprocs, child_run);
 
@@ -541,17 +541,17 @@ static void process_opts(int argc, const char **argv)
 	}
 
 	if (options.machine_readable) {
-		printf(";%g;%d;%d;%.03f;\n", 
-			       throughput,
-			       options.nprocs*options.clients_per_process,
-			       options.nprocs, latency*1000);
+		printf(";%g;%d;%d;%.03f;\n",
+			throughput,
+			options.nprocs*options.clients_per_process,
+			options.nprocs, latency*1000);
 	} else {
-		printf("Throughput %g MB/sec%s%s  %d clients  %d procs  max_latency=%.03f ms\n", 
-			       throughput,
-			       options.sync_open ? " (sync open)" : "",
-			       options.sync_dirs ? " (sync dirs)" : "", 
-			       options.nprocs*options.clients_per_process,
-			       options.nprocs, latency*1000);
+		printf("Throughput %g MB/sec%s%s  %d clients  %d procs  max_latency=%.03f ms\n",
+			throughput,
+			options.sync_open ? " (sync open)" : "",
+			options.sync_dirs ? " (sync dirs)" : "",
+			options.nprocs*options.clients_per_process,
+			options.nprocs, latency*1000);
 	}
 	return 0;
 }
